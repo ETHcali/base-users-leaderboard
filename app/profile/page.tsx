feaf6ed0bbbb8a8a4043c5d47072dea2da1a5989
wmb81321 <wmb81321@gmail.com>
2026-04-21 22:41:24 -0500

    fixing image issue

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react'
+import Image from 'next/image'
 import Link from 'next/link'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import { useAccount } from 'wagmi'
